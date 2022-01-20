4c6b34e7f4bc8e60da1b8da221bf164f252cf5bd
unknown <mperkovic@protonmail.com>
2022-01-20 18:39:12 +0100

    add geocoding api

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=f459f4c41ce7f93f765c82a0f2bf21be&query=37.8267,-122.4233';
+const url = '';
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current.temperature);
+request(
+    {
+        url: url,
+        json: true
+    }, 
+    (error, response) => {
+        if(error) {
+            console.log('Unable to connect to weather service!');
+            return;
+        }
+
+        if(response.body.error) {
+            console.log('Unable to find location!');
+            return;
+        }
+
+        const { temperature, precip, weather_descriptions } = response.body.current;
+        
+        console.log(`${weather_descriptions[0]}. It is currently ${temperature} degrees out. There is ${precip}% chance of rain.`);
 });
 
+const geocodingUrl = '';
+
+request(
+    {
+        url: geocodingUrl,
+        json: true
+    }, 
+    (error, response) => {
+        if(error) {
+            console.log('Unable to connect to location services!');
+            return;
+        }
+        if(!response.body.features[0]) {
+            console.log('Unable to find location!');
+            return;
+        }
+
+        const { center } = response.body.features[0];
+        
+        console.log(center);
+});
